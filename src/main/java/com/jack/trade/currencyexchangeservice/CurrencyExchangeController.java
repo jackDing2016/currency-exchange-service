af9470182165df5279990e00862c959b0264aab3
jack <jackding2015@gmail.com>
2021-09-23 23:52:45 -0400

    commit

---
+++ src/main/java/com/jack/trade/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,31 @@
+package com.jack.trade.currencyexchangeservice;
+
+import com.feignInterface.ExchangeValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.concurrent.ThreadLocalRandom;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private Environment environment;
+
+    private static Logger logger = LoggerFactory.getLogger( CurrencyExchangeController.class );
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)  //from map to USD and to map to INR
+    {
+        logger.info( "retrieveExchangeValue method" );
+        int x = ThreadLocalRandom.current().nextInt(1, 100 );
+        return new  ExchangeValue(1000L, from, to, BigDecimal.valueOf( x ), Integer.parseInt( environment.getProperty( "local.server.port" ) ));
+    }
+
+}
---
+++ src/test/java/com/jack/trade/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jack.trade.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
